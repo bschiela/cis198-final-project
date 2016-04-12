fa063b67298a5f6bdc37a205d22972ed37c8435e
William Schiela <bschiela@gmail.com>
2016-04-11 21:37:20 -0400

    added hyper client to the struct, implemented new and set_region

--- src/ecs_client.rs
+++ src/ecs_client.rs
@@ -2,7 +2,24 @@
 //! TODO provide examples
 
 use aws_types::region::Region;
+use hyper;
 
 pub struct ECSClient {
-    region: Region
+    region: Region,
+    client: hyper::Client
+}
+
+impl ECSClient {
+    /// creates a new ECSClient for the specified Region
+    pub fn new(region: Region) -> ECSClient {
+        ECSClient {
+            region: region,
+            client: hyper::Client::new()
+        }
+    }
+
+    /// sets the Region to which the client sends requests
+    pub fn set_region(&mut self, region: Region) {
+        self.region = region;
+    }
 }
