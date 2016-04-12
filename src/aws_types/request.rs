aaec5a54cbe1b52b4eeeed61fdd8e9c2d0ffa460
William Schiela <bschiela@gmail.com>
2016-04-12 15:42:22 -0400

    under construction...move out of borrowed content errors

--- src/aws_types/request.rs
+++ src/aws_types/request.rs
@@ -1,8 +1,78 @@
 //! A general, Signature Version 4 Signable AWS Request
 
-use hyper::client::RequestBuilder;
+use std::cell::RefCell;
+use std::rc::Rc;
 
-pub struct AWSRequest<'a, T> {
-    request: RequestBuilder<'a>,
-    body_params: T
+use hyper::client::RequestBuilder;
+use hyper::header::{Host, AcceptEncoding, ContentType};
+use hyper::mime::{Mime, TopLevel, SubLevel};
+
+use aws_types::region::Region;
+
+/// The MIME sublevel content type of an ECS HTTP request body
+const AMZ_SUBLEVEL_CONTENT_TYPE: &'static str = "x-amz-json-1.1";
+
+/// A trait defining default implementations for setting headers common to all ECS requests and
+/// signing requests using Amazon's Authorization Version 4 Signing Algorithm
+trait AWSRequest {
+    /// The particular action associated with this request
+    type Action;
+    /// Gets the RequestBuilder object from the particular request type
+    fn get_mut_request_builder(&mut self) -> Rc<RefCell<RequestBuilder>>;
+
+    /// Gets the region that this request is being sent to
+    fn get_region(&self) -> Region;
+
+    /// Sets the Host header
+    fn set_host_header(&mut self) {
+        let region = self.get_region();
+        let mut hostname = String::from("ecs.");
+        hostname.push_str(&region.to_string());
+        hostname.push_str(".amazonaws.com");
+
+        let wrapped_req_builder = self.get_mut_request_builder().clone();
+        let req_builder = wrapped_req_builder.borrow_mut();
+        req_builder.header(
+            Host {
+                hostname: hostname,
+                port: None,
+            }
+        );
+    }
+
+    /// Sets the Accept-Encoding header
+    fn set_accept_encoding_header(&mut self) {
+        let wrapped_req_builder = self.get_mut_request_builder().clone();
+        let req_builder = wrapped_req_builder.borrow_mut();
+        req_builder.header(AcceptEncoding(vec![]));
+    }
+
+    // TODO: set content-length header immediately after setting body
+
+    /// Sets the X-Amz-Target header
+    fn set_x_amz_target_header(&mut self) {
+        unimplemented!()
+    }
+
+    fn set_x_amz_date_header(&mut self) {
+        unimplemented!()
+    }
+
+    fn set_content_type_header(&mut self) {
+        let wrapped_req_builder = self.get_mut_request_builder().clone();
+        let req_builder = wrapped_req_builder.borrow_mut();
+        req_builder.header(
+            ContentType(
+                Mime(
+                    TopLevel::Application,
+                    SubLevel::Ext(String::from(AMZ_SUBLEVEL_CONTENT_TYPE)),
+                    vec![],
+                )
+            )
+        );
+    }
+
+    fn set_authorization_header(&mut self) {
+        unimplemented!()
+    }
 }
