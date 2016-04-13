382daab9b107d5d1329da9876b1d9b7f16d1fff7
William Schiela <bschiela@gmail.com>
2016-04-13 11:47:56 -0400

    re-generated docs with the correct command from assignment page

--- ecs_client/request/sidebar-items.js
+++ ecs_client/request/sidebar-items.js
@@ -1,1 +1,1 @@
-initSidebarItems({"struct":[["ECSRequest",""]]});
+initSidebarItems({"constant":[["AMZ_SUBLEVEL_CONTENT_TYPE","The MIME sublevel content type of an ECS HTTP request body"]],"struct":[["ECSRequest",""]]});
