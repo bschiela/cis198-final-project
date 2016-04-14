4f2e03bbcb2adc982127cf864d7c8351926241db
William Schiela <bschiela@gmail.com>
2016-04-14 13:00:50 -0400

    ECS expects camelCased fields

--- src/request/list_clusters.rs
+++ src/request/list_clusters.rs
@@ -4,11 +4,14 @@
 use request::ecs_request::ECSRequest;
 
 pub struct ListClustersRequest {
-    /// must be between 1 and 100, inclusive; defaults to 100 if missing or invalid
-    max_results: Option<u8>,
-    /// an optional token returned by a previous ListClusters request indicating where to start
-    /// the next page of paginated output (if there are more results than max_results)
-    next_token: Option<String>,
+    /// The max number of cluster results returned in paginated output. 
+    /// Must be between 1 and 100, inclusive.
+    /// If omitted, defaults to 100.
+    maxResults: Option<u8>,
+    /// The value returned from a previous paginated request. 
+    /// Pagination continues from the end of the previous results that returned the value.
+    /// The value returned is null if there are no more results to return.
+    nextToken: Option<String>,
 }
 
 impl ECSRequest for ListClustersRequest {}
