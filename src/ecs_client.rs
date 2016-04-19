00f1dec495fe4bc1e2c19c56090b76475bc790b5
William Schiela <bschiela@gmail.com>
2016-04-19 18:39:50 -0400

    built canonical request, testing now

--- src/ecs_client.rs
+++ src/ecs_client.rs
@@ -7,7 +7,7 @@ use custom_headers::{XAmzTarget, XAmzDate};
 
 use hyper;
 use hyper::client::RequestBuilder;
-use hyper::header::{Headers, Header, Host, AcceptEncoding, Encoding, qitem, ContentType, ContentLength};
+use hyper::header::{Headers, Header, HeaderFormat, Host, AcceptEncoding, Encoding, qitem, ContentType, ContentLength};
 use hyper::mime::{Mime, TopLevel, SubLevel};
 
 use serde_json;
@@ -106,32 +106,65 @@ impl ECSClient {
         target
     }
 
-    fn build_canonical_request(headers: &Headers) -> String {
+    fn build_canonical_request(&self, headers: &Headers) -> String {
         let mut canon_req = String::from("POST\n");
         canon_req.push_str("/\n"); // canonical URI (empty)
         canon_req.push_str("\n"); // canonical query string (empty)
-        // CANONICAL HEADERS
-        // accept encoding
-        let mut ae_string = AcceptEncoding::header_name().to_lowercase();
-        ae_string.push_str(":");
-        let &AcceptEncoding(ref qitem_vec) = headers.get().unwrap();
-        for qitem in qitem_vec {
-            ae_string.push_str(&qitem.to_string());
-        }
-        canon_req.push_str(&ae_string);
-        canon_req.push_str("\n");
-        // content-length
-        let mut cl_string = ContentLength::header_name().to_lowercase();
-        cl_string.push_str(":");
+        // canonical headers
+        // must be sorted lexicographically by lowercase header name
+        let accept_encoding: &AcceptEncoding = headers.get().unwrap();
+        canon_req.push_str(&self.fmt_canonical_header(
+                AcceptEncoding::header_name(),
+                &(accept_encoding as &(HeaderFormat + Send + Sync)).to_string()
+        ));
+        let content_length: &ContentLength = headers.get().unwrap();
+        canon_req.push_str(&self.fmt_canonical_header(
+                ContentLength::header_name(),
+                &(content_length as &(HeaderFormat + Send + Sync)).to_string()
+        ));
+        let content_type: &ContentType = headers.get().unwrap();
+        canon_req.push_str(&self.fmt_canonical_header(
+                ContentType::header_name(),
+                &(content_length as &(HeaderFormat + Send + Sync)).to_string()
+        ));
         let host: &Host = headers.get().unwrap();
-        //canon_req.push_str((*host).to_string());
-        unimplemented!()
+        canon_req.push_str(&self.fmt_canonical_header(
+                Host::header_name(),
+                &(host as &(HeaderFormat + Send + Sync)).to_string()
+        ));
+        let x_amz_date: &XAmzDate = headers.get().unwrap();
+        canon_req.push_str(&self.fmt_canonical_header(
+                XAmzDate::header_name(),
+                &(x_amz_date as &(HeaderFormat + Send + Sync)).to_string()
+        ));
+        let x_amz_target: &XAmzTarget = headers.get().unwrap();
+        canon_req.push_str(&self.fmt_canonical_header(
+                XAmzTarget::header_name(),
+                &(x_amz_target as &(HeaderFormat + Send + Sync)).to_string()
+        ));
+        canon_req
+    }
+
+    fn fmt_canonical_header(&self, name: &str, value: &str) -> String {
+        let mut header = String::from(name).to_lowercase();
+        header.push_str(":");
+        header.push_str(value);
+        // convert sequential spaces to single spaces
+        let mut canon_header = String::new();
+        for token in header.split_whitespace() {
+            canon_header.push_str(token);
+            canon_header.push_str(" ");
+        }
+        // remove trailing space and append newline
+        canon_header = String::from(canon_header.trim_right());
+        canon_header.push_str("\n");
+        canon_header
     }
 }
 
 #[cfg(test)]
 mod test {
-    use hyper::header::{Headers, Host, AcceptEncoding, Encoding, qitem, ContentType, ContentLength};
+    use hyper::header::{Headers, HeaderFormat, Host, AcceptEncoding, Encoding, qitem, ContentType, ContentLength};
     use custom_headers::{XAmzTarget, XAmzDate};
     use time;
     use hyper::mime::{Mime, TopLevel, SubLevel};
@@ -159,9 +192,12 @@ mod test {
         println!("{}", host_val.hostname);
         let xamz_date: &XAmzDate = headers.get().unwrap();
         let date_val = &xamz_date.0;
+        let acc_enc: &AcceptEncoding = headers.get().unwrap();
         println!("{:?}", xamz_date);
         println!("{}", date_val);
         println!("\n");
-        println!("{}", headers)
+        println!("{}", headers);
+        println!("{}", host_val as &(HeaderFormat + Send + Sync));
+        println!("{}", acc_enc as &(HeaderFormat + Send + Sync));
     }
 }
