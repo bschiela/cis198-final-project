f98fde42a1999550f96538fc72eceb9c9bfd30ea
William Schiela <bschiela@gmail.com>
2016-04-19 21:14:18 -0400

    switched to nightly, using combined stable/nightly approach described by serde

--- build.rs
+++ build.rs
@@ -1,17 +1,29 @@
-extern crate syntex;
-extern crate serde_codegen;
+#[cfg(not(feature = "serde_macros"))]
+mod inner {
+    extern crate syntex;
+    extern crate serde_codegen;
 
-use std::env;
-use std::path::Path;
+    use std::env;
+    use std::path::Path;
 
-pub fn main() {
-    let out_dir = env::var_os("OUT_DIR").unwrap();
+    pub fn main() {
+        let out_dir = env::var_os("OUT_DIR").unwrap();
 
-    let src = Path::new("src/lib.rs.in");
-    let dst = Path::new(&out_dir).join("lib.rs");
+        let src = Path::new("src/lib.rs.in");
+        let dst = Path::new(&out_dir).join("lib.rs");
 
-    let mut registry = syntex::Registry::new();
+        let mut registry = syntex::Registry::new();
 
-    serde_codegen::register(&mut registry);
-    registry.expand("", &src, &dst).unwrap();
+        serde_codegen::register(&mut registry);
+        registry.expand("", &src, &dst).unwrap();
+    }
+}
+
+#[cfg(feature = "serde_macros")]
+mod inner {
+    pub fn main() {}
+}
+
+fn main() {
+    inner::main();
 }
--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,13 @@
+#![cfg_attr(feature = "serde_macros", feature(custom_derive, plugin))]
+#![cfg_attr(feature = "serde_macros", plugin(serde_macros))]
+
 extern crate serde;
 extern crate serde_json;
 #[macro_use] extern crate hyper;
 extern crate time;
 
-// required to use serde on stable rust
+#[cfg(feature = "serde_macros")]
+include!("lib.rs.in");
+
+#[cfg(not(feature = "serde_macros"))]
 include!(concat!(env!("OUT_DIR"), "/lib.rs"));
