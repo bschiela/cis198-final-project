ec41880eca32df102f7d0b89c7a63831d28ad303
William Schiela <bschiela@gmail.com>
2016-04-27 15:43:19 -0400

    debugging integration test

--- tests/lib.rs
+++ tests/lib.rs
@@ -1,6 +1,13 @@
 extern crate ecs_client;
 
+use ecs_client::ecs_client::ECSClient;
+use ecs_client::action::list_clusters;
+use ecs_client::region::Region;
+
 #[test]
 fn test_integration_tests_run() {
-    println!("Integration Testing!")
+    let ecs_client = ECSClient::for_region(Region::USEast1);
+    let request = list_clusters::ListClustersRequest::new();
+    let response = ecs_client.list_clusters(request);
+    println!("got response...\n{:?}", response);
 }
