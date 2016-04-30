c18abd1177b0b8a01d8e7c6ad0aeb65968d0dac3
William Schiela <bschiela@gmail.com>
2016-04-30 18:37:52 -0400

    moved docs to crate-level

--- src/ecs_client.rs
+++ src/ecs_client.rs
@@ -1,39 +1,4 @@
-//! This module contains the ECSClient which can be used to interact with Amazon ECS's API.
-//! The client sources your AWS credentials from the environment in which it is running.  You'll
-//! need to create an AWS IAM user and add them to a security group with proper permissions, as
-//! outlined here: http://docs.aws.amazon.com/AmazonECS/latest/developerguide/get-set-up-for-amazon-ecs.html .
-//! Once you've created an IAM user, you can generate security credentials by going to the IAM
-//! console, clicking on Users in the navigation pane, selecting your user from the list, choosing
-//! the Security Credentials tab, and clicking Create Access Key.  This will generate an AWS access
-//! key ID and an AWS secret access key.  Store these in the environment in which your client will
-//! be running by setting the AWS_ACCESS_KEY_ID and AWS_SECRET_ACCESS_KEY, respectively, as such:
-//!
-//! ```text
-//! $ export AWS_ACCESS_KEY_ID="your_access_key_id"
-//! $ export AWS_SECRET_ACCESS_KEY="your_secret_access_key"
-//! ```
-//!
-//! With these credentials created and stored appropriately, the ECSClient should be able to make
-//! requests to Amazon ECS on your behalf.  Simply create an ECSClient, build a request, and call
-//! the relevent API action on the client.  For example (note that the code below should panic if
-//! no security credentials have been stored in the environment):
-//!
-//! ```should_panic
-//! extern crate ecs_client;
-//!
-//! use ecs_client::ecs_client::ECSClient;
-//! use ecs_client::region::Region;
-//! use ecs_client::action::*;
-//!
-//! fn main() {
-//!     let client = ECSClient::for_region(Region::USWest2);
-//!     let request = list_clusters::ListClustersRequest::new();
-//!     let response: list_clusters::ListClustersResponse = ecs_client.list_clusters(request);
-//!     for cluster_arn in response.get_cluster_arns() {
-//!         println!("{}", cluster_arn);
-//!     }
-//! }
-//! ```
+//! This module contains the ECSClient which can be used to make API requests to Amazon ECS.
 
 use region::Region;
 use action::*;
